0d0955b30e585531f4438a629430ed2714be04d8
Karl Kruusamäe <karl.kruusamae@ut.ee>
2019-08-29 17:16:31 +0300

    demo for joint-space goal

---
+++ src/joint_value_goal.cpp
@@ -0,0 +1,89 @@
+/*
+Copyright 2019, University of Tartu
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* Author: Karl Kruusamäe */
+/* E-mail: karl.kruusamae@ut.ee */
+
+#include <ros/ros.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+  // Basic ROS setup
+  // ^^^^^^^^^^^^^^^
+  ros::init(argc, argv, "joint_value_goal");
+  ros::NodeHandle node_handle;
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  // MoveGroupInterface API can be found at:
+  // http://docs.ros.org/kinetic/api/moveit_ros_planning_interface/html/classmoveit_1_1planning__interface_1_1MoveGroupInterface.html
+
+  // MoveIt setup
+  // ^^^^^^^^^^^^
+  // A MoveGroupInterface instance (a client for the MoveGroup action) can be easily setup using just the name of the
+  // planning group you would like to control and plan for.
+  moveit::planning_interface::MoveGroupInterface move_group("xarm7");
+
+  // Planning to a joint value goal
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // We can plan a motion for this group by specifying target values for its joints. The MoveGroupInterface API provides
+  // several ways for setting the joint-space goal but in this example a vector containing target values for all the
+  // joints is used.
+
+  // Getting the current joint values of the robot.
+  std::vector<double> joint_values = move_group.getCurrentJointValues();
+
+  // Modifying the 4th joint value to 1.57 radians (90 degrees).
+  // Feel free to modify the joint number, target value, and the number of influenced joints.
+  joint_values[3] = 1.57;
+  // joint_values[0] = -1.57;
+
+  // Setting the joint_values as joint-space goal.
+  move_group.setJointValueTarget(joint_values);
+
+  // Calling the planner to compute the motion plan, which is then stored in my_plan.
+  // Note that we are just planning, not asking MoveGroupInterface to actually move the robot.
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+  moveit::planning_interface::MoveItErrorCode success = move_group.plan(my_plan);
+  if (success)
+  {
+    ROS_INFO("[movegroup_interface_demo/joint_value_goal] Planning OK. Proceeding.");
+  }
+  else
+  {
+    ROS_WARN("[movegroup_interface_demo/joint_value_goal] Planning failed. Shutting Down.");
+    ros::shutdown();
+    return 0;
+  }
+
+  // Executing the computed plan
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // Uncomment the following line to make the robot move according to the computed plan
+  // move_group.execute(my_plan);
+
+  ros::shutdown();
+  return 0;
+}
